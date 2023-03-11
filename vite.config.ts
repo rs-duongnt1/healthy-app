3fff40e6b79796949693823c7e6b85d1265aed67
duongnt1 <duongnt1@runsystem.net>
2023-03-11 14:53:37 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import path from 'path'
+import path from "path";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./webui"),
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  console.log(process.env.VITE_BASE_API_URL)
+  return defineConfig({
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./webui"),
+      },
     },
-  },
-});
+  });
+};
--- webui/services/baseQuery.ts
+++ webui/services/baseQuery.ts
@@ -1,5 +1,5 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8080/api",
+  baseUrl: import.meta.env.VITE_BASE_API_URL,
 });
